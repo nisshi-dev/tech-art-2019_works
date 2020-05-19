8ce0502e33b4cfdba931f32c830dec24e97ee74b
nishidayoshikatsu <nsd24technology@gmail.com>
2020-05-19 20:38:57 +0900

    header用のコンポーネントを作成

---
+++ components/PageHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <h2 class="pageTitle">
+      <slot />
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: flex-end;
+  flex-wrap: wrap;
+}
+
+.pageTitle {
+  color: gray;
+  display: flex;
+  align-items: center;
+  line-height: 1.35;
+  font-weight: normal;
+  margin: 0 0.5em 0 0;
+}
+</style>
