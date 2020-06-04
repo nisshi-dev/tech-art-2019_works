9c6760d3087d263e2e5455097d16df4359f0a710
nishidayoshikatsu <nsd24technology@gmail.com>
2020-06-04 20:58:12 +0900

    cssで大まかな位置を調整

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,32 +1,45 @@
 <template>
   <div class="footer">
-    <p class="credit">
-      <br /><br />
-      Credit Produced by the student team of Creative Engineering
-      Practice（ものづくり創成プロジェクト）, YAMAGUCHI University, and
-      challenge lecture’Technology x Art’,YAMAGUCHI University, Ube Kosen,
-      UBE<br />
-      <b>City Supervisor: </b>Prof. Koshiba Mamiko Ph.D.<br />
-      <b>Associate Professor Creators: </b>Horikawa Yuki / Yamaguchi Marina /
-      Mishima Seitaro / Watanabe Joichiro / Hua Zhudi / Fujita Koji / Kawasaki
-      Hiroki / Maeda Ryo / Kubota Momoka / Kishida Shinji / Komura Shunsuke /
-      Tanabe Reira / Itamura Mizuki / Suzuki Ryosuke / Kevin Jay Singh /
-      Kanazawa Ryo / Sato Ryota / Takata Jumpei / Nakano Tomohiro / Yoshikawa
-      Teru18taka <br />
-      <b>General Support: </b>UBE Tokiwa Park<br />
-      <b>Stuff Supportors: </b>（敬称略）Marumo Yuji / Kumagai Mitsuyuki /
-      Hayakawa Yoshiaki / Shinozaki Keiji / Mitsui Kazuhiko / Fujita Tadao /
-      Nakano Yasuo / Tashiro Koichi / Kaneko Junko/ Shintani Yuko/ Uchida Yaeko
-      / Sugiyama Isamu / Sugiyama Megumi / Hirasa Yoshie /日高 / AC設備 /
-      宇部護国神社 / 山口銀行上宇部支店 / 富士アート / エコー設備管工 /
-      ダイサンプロパン / 田辺仏具展 / 小川蜜カス本舗 / うべ未来100プロジェクト /
-      三和企業 / 福島建設 / 金子小児科 <br />
-      <b>Web: </b>Horikawa Yuki ※Out of order Presented by Ube City
-    </p>
-    <p class="copyright">
-      Copyright ©️2020 Center for Design and Product Innovation All Rights
-      Reserved.
-    </p>
+    <div class="credit">
+      <h4>Credit</h4>
+      <p>
+        Credit Produced by the student team of Creative Engineering
+        Practice（ものづくり創成プロジェクト）, YAMAGUCHI University, and
+        challenge lecture’Technology x Art’,YAMAGUCHI University, Ube Kosen,
+        UBE<br />
+        <b>City Supervisor: </b>Prof. Koshiba Mamiko Ph.D.<br />
+        <b>Associate Professor Creators: </b>Horikawa Yuki / Yamaguchi Marina /
+        Mishima Seitaro / Watanabe Joichiro / Hua Zhudi / Fujita Koji / Kawasaki
+        Hiroki / Maeda Ryo / Kubota Momoka / Kishida Shinji / Komura Shunsuke /
+        Tanabe Reira / Itamura Mizuki / Suzuki Ryosuke / Kevin Jay Singh /
+        Kanazawa Ryo / Sato Ryota / Takata Jumpei / Nakano Tomohiro / Yoshikawa
+        Teru18taka <br />
+        <b>General Support: </b>UBE Tokiwa Park<br />
+        <b>Web: </b>Horikawa Yuki ※Out of order Presented by Ube City
+      </p>
+    </div>
+    <div class="presedted">
+      <h4>Presented by</h4>
+      <p>
+        <b>Stuff Supportors: </b>（敬称略）Marumo Yuji / Kumagai Mitsuyuki /
+        Hayakawa Yoshiaki / Shinozaki Keiji / Mitsui Kazuhiko / Fujita Tadao /
+        Nakano Yasuo / Tashiro Koichi / Kaneko Junko/ Shintani Yuko/ Uchida
+        Yaeko / Sugiyama Isamu / Sugiyama Megumi / Hirasa Yoshie /日高 / AC設備
+        / 宇部護国神社 / 山口銀行上宇部支店 / 富士アート / エコー設備管工 /
+        ダイサンプロパン / 田辺仏具展 / 小川蜜カス本舗 / うべ未来100プロジェクト
+        / 三和企業 / 福島建設 / 金子小児科 <br />
+      </p>
+    </div>
+    <div class="share">
+      <h4>Share</h4>
+      <p>NSD</p>
+    </div>
+    <div class="copyright">
+      <small>
+        Copyright ©️2020 Center for Design and Product Innovation All Rights
+        Reserved.
+      </small>
+    </div>
   </div>
 </template>
 
@@ -34,22 +47,39 @@
 
 <style lang="scss" scoped>
 .footer {
-  //display: flex;
-  align-items: flex-end;
+  display: flex;
+  //align-items: flex-end;
   flex-wrap: wrap;
-  width: 100%;
-  height: 100%;
+  width: auto;
+  border: 1px solid white;
+  color: #dddddd;
+  justify-content: center;
 }
 
 .credit {
-  margin: 1.5rem;
-  color: #dddddd;
-  line-height: 2rem;
+  border: 1px solid white;
+  width: calc(100% / 3);
+  //margin: 1.5rem;
+  //color: #dddddd;
+  //line-height: 2rem;
+}
+
+.presedted {
+  border: 1px solid white;
+  width: calc(100% / 3);
+}
+
+.share {
+  border: 1px solid white;
+  width: calc(100% / 3);
 }
 
 .copyright {
-  display: flex;
-  justify-content: center;
-  color: #dddddd;
+  //display: block;
+  border: 1px solid white;
+  width: 100%;
+  //display: flex;
+  //justify-content: center;
+  //color: #dddddd;
 }
 </style>
