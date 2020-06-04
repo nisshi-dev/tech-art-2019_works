7decddb79b3e8a05359b083e993056306de253a9
nishidayoshikatsu <nsd24technology@gmail.com>
2020-06-05 01:27:44 +0900

    shareボタンを設置

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -32,7 +32,17 @@
     </div>
     <div class="share">
       <h4>Share</h4>
-      <p>NSD</p>
+      <div class="social">
+        <a class="btn btn-icon btn-facebook" href="#"
+          ><i class="fa fa-facebook"></i><span>Facebook</span></a
+        >
+        <a class="btn btn-icon btn-twitter" href="#"
+          ><i class="fa fa-twitter"></i><span>Twitter</span></a
+        >
+        <a class="btn btn-icon btn-googleplus" href="#"
+          ><i class="fa fa-google-plus"></i><span>Google+</span></a
+        >
+      </div>
     </div>
     <div class="copyright">
       <small>
@@ -85,6 +95,107 @@
   //border: 1px solid white;
   width: calc(100% / 3);
   padding-left: 0.5vw;
+  .social {
+    @import url(//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css); // icon
+    margin: 0.3vw;
+    .btn {
+      clear: both;
+      white-space: nowrap;
+      font-size: 0.8em;
+      display: inline-block;
+      border-radius: 5px;
+      box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.35);
+      margin: 2px;
+      -webkit-transition: all 0.5s;
+      -moz-transition: all 0.5s;
+      transition: all 0.5s;
+      overflow: hidden; // はみ出た部分は非表示
+    }
+    .btn:hover {
+      box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.45);
+    }
+    .btn:focus {
+      box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.4);
+    }
+    .btn > span,
+    .btn-icon > i {
+      float: left;
+      padding: 13px 23px 13px 13px;
+      -webkit-transition: all 0.5s;
+      -moz-transition: all 0.5s;
+      transition: all 0.5s;
+      line-height: 1em;
+    }
+
+    .btn > span {
+      padding: 14px 18px 16px;
+      white-space: nowrap;
+      color: #fff;
+      background: #b8b8b8;
+    }
+
+    .btn:focus > span {
+      background: #9a9a9a;
+    }
+
+    .btn-icon > i {
+      border-radius: 5px 0 0 5px;
+      position: relative;
+      width: 13px;
+      text-align: center;
+      font-size: 1.25em;
+      color: #fff;
+      background: #212121;
+    }
+
+    .btn-icon > i:after {
+      content: '';
+      border: 8px solid;
+      border-color: transparent transparent transparent #222;
+      position: absolute;
+      top: 13px;
+      right: -15px;
+    }
+
+    .btn-icon:hover > i,
+    .btn-icon:focus > i {
+      color: #fff;
+    }
+
+    .btn-icon > span {
+      border-radius: 0 5px 5px 0;
+    }
+
+    /*Facebook*/
+    .btn-facebook:hover > i,
+    .btn-facebook:focus > i {
+      color: #3b5998;
+    }
+
+    .btn-facebook > span {
+      background: #3b5998;
+    }
+
+    /*Twitter*/
+    .btn-twitter:hover > i,
+    .btn-twitter:focus > i {
+      color: #55acee;
+    }
+
+    .btn-twitter > span {
+      background: #55acee;
+    }
+
+    /*Google*/
+    .btn-googleplus:hover > i,
+    .btn-googleplus:focus > i {
+      color: #dd4b39;
+    }
+
+    .btn-googleplus > span {
+      background: #dd4b39;
+    }
+  }
 }
 
 .copyright {
