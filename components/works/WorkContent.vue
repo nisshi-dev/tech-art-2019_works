c44d63773625c5e813311c829e3b16f4035e537b
nishidayoshikatsu <nsd24technology@gmail.com>
2020-06-07 06:06:19 +0900

    画像を追加し、配置

--- components/works/WorkContent.vue
+++ components/works/WorkContent.vue
@@ -4,45 +4,45 @@
     <h2>作品集</h2>
     <div class="content">
       <ul>
-        <li class="robot-village">
+        <li class="robot-village w-img">
           <h3>ロボット村</h3>
           <h4>ースマートフォンで実社会づくりー</h4>
-          <!--<img src="~/static/works/robot_village.jpg" />-->
+          <img src="~/static/works/robot_village.jpg" />
         </li>
-        <li class="koushi-house">
+        <li class="koushi-house w-img">
           <h3>KOUSHI HOUSE</h3>
           <h4>―親子―</h4>
-          <!--<img src="~/static/works/KOUSHI_HOUSE.jpg" />-->
+          <img src="~/static/works/KOUSHI_HOUSE.jpg" />
         </li>
-        <li>
-          <h3>音の水槽</h3>
-          <h4>―運動方程式に従い散らばる魚たち―</h4>
-          <!--<img src="~/static/works/KOUSHI_HOUSE.jpg" />-->
-        </li>
-        <li>
+        <li class="light-cube w-img">
           <h3>光る立体</h3>
           <h4>―卓上の星団―</h4>
-          <!--<img src="~/static/works/KOUSHI_HOUSE.jpg" />-->
+          <img src="~/static/works/light_cube.jpg" />
         </li>
-        <li>
-          <h3>小さな自然</h3>
-          <h4>―ささえあう癒し―</h4>
-          <!--<img src="~/static/works/KOUSHI_HOUSE.jpg" />-->
-        </li>
-        <li>
+        <li class="lucky-chance w-img">
           <h3>ラッキーチャンス！！</h3>
           <h4>―ITの神様のお告げ―</h4>
-          <!--<img src="~/static/works/KOUSHI_HOUSE.jpg" />-->
+          <img src="~/static/works/lucky_chance.jpg" />
         </li>
-        <li>
+        <li class="sound-fish h-img">
+          <h3>音の水槽</h3>
+          <h4>―運動方程式に従い散らばる魚たち―</h4>
+          <img src="~/static/works/sound_fish.jpg" />
+        </li>
+        <li class="small-nature h-img">
+          <h3>小さな自然</h3>
+          <h4>―ささえあう癒し―</h4>
+          <img src="~/static/works/small_nature.jpg" />
+        </li>
+        <li class="r h-img">
           <h3>R</h3>
           <h4>―輝かしい時代―</h4>
-          <!--<img src="~/static/works/KOUSHI_HOUSE.jpg" />-->
+          <img src="~/static/works/R.jpg" />
         </li>
-        <li>
+        <li class="kamehame h-img">
           <h3>カメハメ波</h3>
           <h4>―叫んでみよう！―</h4>
-          <!--<img src="~/static/works/KOUSHI_HOUSE.jpg" />-->
+          <img src="~/static/works/kamehame.jpg" />
         </li>
       </ul>
     </div>
@@ -62,30 +62,30 @@ export default {
 
 <style lang="scss" scoped>
 .work-content {
-  border: 1px solid white;
+  //border: 1px solid white;
   color: white;
   margin-top: 1rem;
   display: flex;
-  //width: 100%;
-  //flex-wrap: wrap; // 折返し有効
-  //justify-content: center; // 垂直中央揃え
   align-items: center; // 水平中央揃え
   flex-direction: column; // 縦に整列
   h2 {
-    //justify-content: center; // 水平中央揃え
     text-decoration: underline;
     //border: 1px solid white;
     height: 5vh;
-    //height: auto;
   }
 }
 
+.w-img img {
+  height: 35vh;
+  //width: 30vw;
+}
+
+.h-img img {
+  width: 15vw;
+  height: 50vh;
+}
+
 .content {
-  //display: flex;
-  //flex-direction: row;
-  //align-items: center; // 水平中央揃え
-  //flex-wrap: wrap; // 折返し有効
-  //justify-content: flex-start;
   border: 1px solid white;
   ul {
     display: flex; // listを横並びにする
@@ -100,9 +100,8 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center; // 水平中央揃え
-    width: 300px;
     img {
-      width: 30%;
+      margin-top: 10px;
     }
   }
   li :link {
