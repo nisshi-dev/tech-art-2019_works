cbc8846ef31819086607f3c6072916c2942ad0b5
nishidayoshikatsu <nsd24technology@gmail.com>
2020-06-07 01:27:09 +0900

    作品部分のコードを削除

--- components/WorkContent.vue
+++ components/WorkContent.vue
@@ -2,18 +2,6 @@
   <div class="work-content">
     <bread-crumb />
     <h2>作品集</h2>
-    <div class="robot-village">
-      <img src="~/static/works/KOUSHI_HOUSE.jpg" />
-      <div class="disc">
-        <h3>ロボット村</h3>
-      </div>
-    </div>
-    <div class="koushi-house">
-      <img src="~/static/works/KOUSHI_HOUSE.jpg" />
-      <div class="disc">
-        <h3>KOUSHI HOUSE</h3>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -41,39 +29,9 @@ export default {
   h2 {
     //justify-content: center; // 水平中央揃え
     text-decoration: underline;
-    border: 1px solid white;
+    //border: 1px solid white;
     height: 5vh;
     //height: auto;
-  }
-}
-
-.robot-village {
-  //display: block;
-  display: flex;
-  flex-direction: row;
-  //border: 1px solid white;
-  padding: 13px;
-  border-bottom: 1px solid gray;
-  //width: 0.3vw;
-  img {
-    border: 1px solid white;
-    width: 30%;
-  }
-}
-
-.disc {
-  border: 1px solid white;
-}
-
-.koushi-house {
-  display: flex;
-  flex-direction: row;
-  //border: 1px solid white;
-  padding: 13px;
-  //width: 0.3vw;
-  img {
-    border: 1px solid white;
-    width: 30%;
   }
 }
 </style>
