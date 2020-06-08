511800973dfbee477b4673a395a5326d0fc55a85
nishidayoshikatsu <nsd24technology@gmail.com>
2020-06-08 15:46:00 +0900

    ものづくり創成センターへのリンクを追加

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -3,6 +3,12 @@
     <h2 class="pageTitle Noto-400i">
       <slot />
     </h2>
+    <div class="yamaguchi">
+      <a href="http://www.mono.eng.yamaguchi-u.ac.jp/" class="yama-logo">
+        <img src="~/static/yamaguchi.png" />
+        <p>ものづくり創成センター</p>
+      </a>
+    </div>
     <div class="header-menu Noto-300">
       <ul>
         <li
@@ -27,7 +33,7 @@ export default {
 .header {
   display: flex; // flexboxの指定
   border: 1px solid white;
-  height: 8vh; // headerの高さを指定
+  height: 12vh; // headerの高さを指定
   width: auto;
   //flex-wrap: wrap;
   //justify-content: center;
@@ -45,15 +51,65 @@ export default {
   //align-items: center;
 }
 
+.yamaguchi {
+  //margin-top: 0;
+  //margin-left: auto;
+  border: 1px solid white;
+  height: 40px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: auto; // 上寄せ
+  margin-right: -310px;
+  //margin-left: auto;
+  //margin-left: 5vw;
+  .yama-logo {
+    display: flex;
+    flex-direction: row;
+    //border: 1px solid white;
+    text-decoration: none;
+    //justify-content: flex-end;
+    //margin-left: auto;
+    //padding-top: 15px;
+
+    img {
+      //padding-top: 5px;
+      //margin-right: 3px;
+      //margin-bottom: 10px;
+      //margin-bottom: 10px;
+      width: 20px;
+      height: 22px;
+    }
+
+    p {
+      //padding-top: -11px;
+      color: white;
+    }
+  }
+}
+
+.yama-logo p :link {
+  color: white;
+  text-decoration: none;
+}
+
+.yama-logo p :visited {
+  color: white;
+  text-decoration: none;
+}
+
 .header-menu {
   color: gray;
   border: 1px solid white;
   //height: 4vh;
+  margin-right: 10px;
   margin-top: auto; // 下寄せ
   ul {
     display: flex; // listを横並びにする
     list-style-type: none; // リストの接頭部を削除
     padding-left: 0;
+    //margin-right: 10vw;
   }
   .header-navlink {
     border: solid white;
